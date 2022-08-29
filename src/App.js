9434c679311e7be60b62a16c72c979bd650c7d10
unknown <slupiktomasz@gmail.com>
2022-08-29 07:49:30 +0200

    add

--- src/App.js
+++ src/App.js
@@ -5,19 +5,68 @@ import Second from "./components/second";
 import Third from "./components/third";
 import Fourth from "./components/fourth";
 
+function Zaloguj() {
+	return <h1>Zaloguj się</h1>;
+}
+
+function Wyloguj() {
+	return <h1>Wyloguj się</h1>;
+}
+
+function LoginApp(props) {
+	if (props.zalogowany) {
+		return <Wyloguj />;
+	} else {
+		return <Zaloguj />;
+	}
+}
 
 function App() {
 	let myname = "Tomek";
-	let tri = 'Triathlon'
-	let dish = 'Pierogi'
-	let drink = 'Fantę'
+	let tri = "Triathlon";
+	let dish = "Pierogi";
+	let drink = "Fantę";
+	let mycity = "Warszawa";
+	let numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+
+	let sports = [
+		{id:1, namesport: "⚽️"}, 
+		{id:2, namesport: "🤼‍♀️"}, 
+		{id:3, namesport: "🏇"}, 
+		{id:4, namesport: "⛷"}, 
+		]
+
+	let listsports = sports.map (sport => {
+		return <h3 key={sport.id}>{sport.namesport}</h3>
+	});
+
 
 	return (
 		<div>
 			<First imie={myname} />
 			<Second imie={myname} sports={tri} />
-			<Third imie={myname} meal={dish} />	
-			<Fourth item={drink}/>
+			<Third imie={myname} meal={dish} />
+			<Fourth item={drink} city={mycity} />
+			<h2>Instrukcja warunkowa</h2>
+			{tri === "Triathlon" ? "Tak to prawa" : "Nie"}
+
+			{dish === "Pierogi" ? (
+				<h1>To jest moje ulubione danie </h1>
+			) : (
+				<h1>Nie</h1>
+			)}
+
+			<div>
+				<LoginApp zalogowany={false}></LoginApp>
+			</div>
+
+			<div>
+				{numbers.map((number) => {
+					return <h3>{number}</h3>;
+				})}
+
+			</div>
+			<div>{listsports}</div>
 		</div>
 	);
 }
--- src/components/fourth.js
+++ src/components/fourth.js
@@ -1,20 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Fourth(props) {
 	const [item, setItem] = useState("Fantę");
-    const [data] = useState(new Date())
+	const [data] = useState(new Date());
+	const [city, setCity] = useState("");
 
+	useEffect(() => {
+		console.log("Początek komponentu");
+		setCity(props.city);
 
+		return () => {
+			console.log("Koniec komponentu");
+		};
+	}, []);
 
-	const clickItem = () => {
+	const clickItem = (evt) => {
 		setItem("Cola Zero");
 	};
 
 	return (
 		<div>
 			<div>W ciepłe dni uwielbiam pić zimną {item}</div>
-            <div onClick={clickItem}>Zmiana napoju, Kliknij tutaj</div>
-            <h3>Dzisiejsza data {data.toString().split("T")}</h3>
+			<div onClick={clickItem}>Zmiana napoju, Kliknij tutaj</div>
+			<h3>Dzisiejsza data {data.toString().split("T")}</h3>
+			<div> Miasto: {city} </div>
 		</div>
 	);
 }
